71f7a1f39fb0d199aa9ff7c708fb4ac2dfe0e2de
ismaelmaizon <ismael.maizon@gmail.com>
2024-09-30 22:49:36 -0300

    mejorando cookie

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -54,8 +54,10 @@ export const login = async (req, res) => {
         }
         console.log(token);
         const cookiesOptions = {
-            maxAge: 21600000, 
-            httpOnly: true    
+            maxAge: 21600000,
+            httpOnly: true,
+            secure: true, // Solo HTTPS
+            sameSite: 'Lax' // Mitigar ataques CSRF  
         }
         res.cookie('jwt', token, cookiesOptions)
         res.send({status: 200, message: 'Bien venido', response: tockenRol})
